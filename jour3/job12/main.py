d9e097114609b92c2909856f03ed4725fe3ca0f4
lucas-iribaren <lucas.iribaren@laplateforme.io>
2023-11-22 17:59:15 +0100

    aller plus loin

--- jour3/job12/main.py
+++ jour3/job12/main.py
@@ -1,2 +1,20 @@
 def chaine_inverse(text):
-    
+    string = ""
+    i = len(text)-1
+    while (i >= 0):
+        string += text[i]
+        i -= 1
+    return string
+print(chaine_inverse("nikana"))
+
+def bis(x):
+    res = ''
+    for k in range(len(x)-1, -1, -1):
+        res += x[k]
+    return res
+
+print(bis("live"))
+
+def bis2(y):
+    return y[::-1]
+print(bis2("stressed")) 
