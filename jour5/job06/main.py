644fb438c36c27996b037512fb7d8cf2fe025f05
lucas-iribaren <lucas.iribaren@laplateforme.io>
2023-11-27 16:44:03 +0100

    jour 5 - runtrack python - job06

--- jour5/job06/main.py
+++ jour5/job06/main.py
@@ -1,6 +1,11 @@
-def distance_to_toilettes(nombre_marches, hauteur_marche):
-    distance_totale_par_jour = 5 * (hauteur_marche/100) * 2 * nombre_marches
-    distance_totale_par_semaine = distance_totale_par_jour * 7
-    print (f"Pour {nombre_marches} marches de {hauteur_marche} cm, le gardien parcourt {distance_totale_par_semaine:.2f} m par semaine.")
+nombre_marches= int(input('Veuillez rentrer le nombre de marche:'))
+hauteur_marche= int(input("Veuillez rentrer la hauteur d'une marche:"))
+distance_totale=0
 
-distance_to_toilettes(300, 30)
+def distance_toilette(nombre_marches, hauteur_marche):
+    distance_totale =(nombre_marches * hauteur_marche * 2 * 5 * 7)/100
+    return distance_totale 
+
+
+resultat= distance_toilette(nombre_marches, hauteur_marche)
+print (f"Pour {nombre_marches} marches de {hauteur_marche} cm, le gardien parcourt {resultat} m par semaine.")
